3d3640d90bb127d71023e874e29faf9ce0cc0a02
justyn1303 <justyn1303@gmail.com>
2022-11-16 01:19:16 +0100

    ini comm

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
-import data from "./data.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -16,9 +17,8 @@ mongoose
 
 const app = express();
 
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
